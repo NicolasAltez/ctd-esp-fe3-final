353baf5132b95a5fad043abc6949f12eccf079c5
Nicolas Altez <nicolasaltezacosta27@gmail.com>
2024-06-14 13:40:54 -0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 
+import { useContext } from "react";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./Routes/Home";
+import Contact from "./Routes/Contact";
+import Favs from "./Routes/Favs";
+import DentistDetail from "./Routes/Detail";
+import { ContextGlobal } from "./Components/utils/global.context";
+
 
 
 function App() {
+  const {state} = useContext(ContextGlobal);
   return (
-      <div className="App">
-          <Navbar/>
-          <Footer/>
-      </div>
+    <div className={state.theme === 'dark' ? 'dark' : 'light'}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/favs" element={<Favs />} />
+          <Route path="/dentist/:id" element={<DentistDetail />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
   );
 }
 
