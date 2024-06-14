92b26ad79ecbed103bceda0eae4926629a2e06c9
Nicolas Altez <nicolasaltezacosta27@gmail.com>
2024-06-14 13:39:05 -0300

    gobal context

--- src/Components/utils/global.context.jsx
+++ src/Components/utils/global.context.jsx
@@ -1,14 +1,30 @@
-import { createContext } from "react";
-
-export const initialState = {theme: "", data: []}
+import React, { createContext, useReducer, useMemo, useEffect } from 'react';
+import axios from 'axios';
+import { reducer, initialState } from '../../reducers/reducer';
 
 export const ContextGlobal = createContext(undefined);
 
 export const ContextProvider = ({ children }) => {
-  //Aqui deberan implementar la logica propia del Context, utilizando el hook useMemo
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(res => {
+        dispatch({ type: 'SET_DENTISTS', payload: res.data });
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  const value = useMemo(() => ({
+    state,
+    dispatch,
+    toggleTheme: () => {
+      dispatch({ type: 'SET_THEME', payload: state.theme === 'light' ? 'dark' : 'light' });
+    },
+  }), [state]);
 
   return (
-    <ContextGlobal.Provider value={{}}>
+    <ContextGlobal.Provider value={value}>
       {children}
     </ContextGlobal.Provider>
   );
