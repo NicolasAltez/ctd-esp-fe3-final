cd6f1d5ed54f8f2bc7c765a2da4cbfc3ac986f4e
Nicolas Altez <nicolasaltezacosta27@gmail.com>
2024-06-14 20:31:05 -0300

    global context is modified

--- src/Components/utils/global.context.jsx
+++ src/Components/utils/global.context.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useReducer, useMemo, useEffect } from 'react';
-import axios from 'axios';
+import { fetchDentists } from '../../services/api';
 import { reducer, initialState } from '../../reducers/reducer';
 
 export const ContextGlobal = createContext(undefined);
@@ -8,11 +8,16 @@ export const ContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/users')
-      .then(res => {
-        dispatch({ type: 'SET_DENTISTS', payload: res.data });
-      })
-      .catch(err => console.log(err));
+    const getDentists = async () => {
+      try {
+        const data = await fetchDentists();
+        dispatch({ type: 'SET_DENTISTS', payload: data });
+      } catch (error) {
+        console.error('Error fetching dentists:', error);
+      }
+    };
+
+    getDentists();
   }, []);
 
   const value = useMemo(() => ({
@@ -21,7 +26,25 @@ export const ContextProvider = ({ children }) => {
     toggleTheme: () => {
       dispatch({ type: 'SET_THEME', payload: state.theme === 'light' ? 'dark' : 'light' });
     },
+    addFavorite: (dentist) =>{
+      const updatedFavorites = [...state.favorites, dentist];
+      localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
+      dispatch({ type: 'SET_FAVORITES', payload: updatedFavorites });
+    },
+    removeFavorite: (id) => {
+      const updatedFavorites = state.favorites.filter(dentist => dentist.id !== id);
+      localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
+      dispatch({ type: 'SET_FAVORITES', payload: updatedFavorites });
+    },
+    loadFavorites: () => {
+      const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+      dispatch({ type: 'SET_FAVORITES', payload: favorites });
+    }
   }), [state]);
+
+  useEffect(() => {
+    value.loadFavorites();
+  }, []);
 
   return (
     <ContextGlobal.Provider value={value}>
